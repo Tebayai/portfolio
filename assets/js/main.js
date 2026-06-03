786a69ed479b30b0346c8d783be487ba28fc2239
Tebayai <yvannerick@gmail.com>
2026-06-03 21:57:16 +0200

    add design header

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,3 +3,13 @@
 // ===================================================
 
 'use strict';
+
+const nav = document.querySelector('.nav');
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 0) {
+        nav.classList.add('scrolled');
+    } else {
+        nav.classList.remove('scrolled');
+    }
+});
