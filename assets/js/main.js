f26ef3dec95b1873bb5454e77908d924a8d78686
Tebayai <yvannerick@gmail.com>
2026-06-04 14:50:52 +0200

    header 80%

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,7 @@
 
 'use strict';
 
+// Change nav background on scroll
 const nav = document.querySelector('.nav');
 
 window.addEventListener('scroll', () => {
@@ -13,3 +14,21 @@ window.addEventListener('scroll', () => {
         nav.classList.remove('scrolled');
     }
 });
+
+// Toggle mobile menu
+const openMenu = document.querySelector('.nav_toggle');
+
+openMenu.addEventListener('click', () =>{
+    const navList = document.querySelector('.nav_list');
+    const closeicon = document.querySelector('.nav_toggle i');
+
+    if (navList.classList.contains('active')) {
+        closeicon.classList.remove('fa-xmark');
+        closeicon.classList.add('fa-bars');
+    } else {
+        closeicon.classList.remove('fa-bars');
+        closeicon.classList.add('fa-xmark');
+    }
+
+    navList.classList.toggle('active');
+})
